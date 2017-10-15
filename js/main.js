7047b4a1201b44761dc207fd0237ea01367a9014
ZiluoH <ziluoh@gmail.com>
2017-10-14 20:19:46 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-*
+// document.getElementById('zipInput').oncubmit = function(){lo()}
+
+// function lo() {
+	
+// 	console.log('lll')
+
+
+// }
+
+function lo(){
+	console.log('hit')
+}
+
+
+document.getElementById('zipInput').onkeydown = function(e){
+	if (e.keyCode == 13) {
+		lo()
+	}
+}
