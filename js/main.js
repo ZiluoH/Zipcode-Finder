92be9f7be187ee6e87d6d946d9629e8673a34ddb
ZiluoH <ziluoh@gmail.com>
2017-10-16 04:06:11 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,18 +5,42 @@ function getLocation(){
 
 	// make api request
 	fetch('http://api.zippopotam.us/us/' + zip)
-		.then(response => {
-			// if status = 200, which means zip code is good
-			if (response.status != 200) {
-				document.getElementById('result').innerHTML = error
+		.then(
+			function(response){
+			if (response.status !== 200) {
+				// Fail
+				// show error msg if zip code not found and hide info section
+				document.getElementById('errorMsg').classList.remove('hide')
+				document.getElementById('locationInfo').classList.add('hide')
+				console.log('fail')
 			} else {
-				return response.json()
+				// success
+				// show location info and hide error msg is zi code is found
+				document.getElementById('errorMsg').classList.add('hide')
+				document.getElementById('locationInfo').classList.remove('hide')
+				console.log('success')
+				// edit data we get
+				response.json().then(function(data){
+					console.log(data)
+					// store data place info to zipData
+					var zipData = data.places[0]
+					// there is a space in 'place name', need to use brackets to define it
+					// var zipPlaceName = data.places[0]['place name']
+					var zipInfo = document.getElementById('zipInfo')
+					var zipPlaceName = document.createElement('li')
+					var zipState = document.createElement('li')
+					var zipLongitude = document.createElement('li')
+					var zipLatitude = document.createElement('li')
+					
+					zipPlaceName.innerText = "City: " + zipData['place name']
+					
+					zipInfo.appendChild(zipPlaceName)
+					
+				})
 			}
-		})
-		.then(data => {
-			// show location info
 			
 		})
+
 }
 
 
@@ -27,4 +51,3 @@ document.getElementById('zipInput').onkeydown = function(e){
 	}
 }
 
-var error = "wrongsdjfsldkjflksdj"
