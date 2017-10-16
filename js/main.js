a453c3e652fca548070073cb8e0c7ed6f92292b4
ZiluoH <ziluoh@gmail.com>
2017-10-15 18:51:11 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
-// document.getElementById('zipInput').oncubmit = function(){lo()}
+function getLocation(){
+	// get zip code from input
+	const zip = document.getElementById('zipInput').value
+	console.log(zip)
 
-// function lo() {
-	
-// 	console.log('lll')
-
-
-// }
-
-function lo(){
-	console.log('hit')
+	// make api request
+	fetch('http://api.zippopotam.us/us/' + zip)
+		.then(response => {
+			// if status = 200, which means zip code is good
+			if (response.status != 200) {
+				document.getElementById('result').innerHTML = error
+			} else {
+				return response.json()
+			}
+		})
+		.then(data => {
+			// show location info
+			
+		})
 }
 
 
+// press enter run function getLocation
 document.getElementById('zipInput').onkeydown = function(e){
 	if (e.keyCode == 13) {
-		lo()
+		getLocation()
 	}
 }
+
+var error = "wrongsdjfsldkjflksdj"
